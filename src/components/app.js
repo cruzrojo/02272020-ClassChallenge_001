69ee7f5435bd5b175d1a0aa9099649acf4f3ad08
cruzrojo <burkerich@gmail.com>
2020-02-27 10:34:52 -0700

    second commit. Added a nav-bar.js element. Imported it into the app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import NavBar from './navigation/nav-bar'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Skeleton</h2>
+        <h1>Burke M Rich Contacts Page</h1>
+        <h2>Hi! How are you!</h2>
+        <NavBar />
       </div>
     );
   }
---
+++ src/components/navigation/nav-bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+        <div className="right-column">
+            <h1>Hello from App</h1>
+        </div>
+        );
+      }
+    }
